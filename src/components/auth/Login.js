93b6f0445e7aef5dd806e9d9b065269cb19fc5a6
Dustin Murdock <dmurdock2@gmail.com>
2020-05-26 09:53:54 -0500

    changed Login form to username and password

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -33,10 +33,10 @@ const Login = props => {
             <form className="form--login" onSubmit={handleLogin}>
                 <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
                 <fieldset>
-                    <label htmlFor="inputEmail"> Email address </label>
+                    <label htmlFor="inputUsername"> Username </label>
                     <input ref={username} type="username"
                         className="form-control"
-                        placeholder="Email address"
+                        placeholder="username"
                         required autoFocus />
                 </fieldset>
                 <fieldset>
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -31,10 +31,10 @@ const NavBar = props => {
                         </li> :
                         <>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login/">Login</Link>
+                            <Link className="nav-link" to="/login">Login</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/register/">Register</Link>
+                            <Link className="nav-link" to="/register">Register</Link>
                         </li>
                         </>
                 }
