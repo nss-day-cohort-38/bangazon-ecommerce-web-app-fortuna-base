f1f6c8012b6816fbd614a71756e1ae9f5b427706
Cody Murdock <warhammer4563@gmail.com>
2020-05-28 09:16:59 -0500

    completed add payment form

--- src/modules/PaymentTypeManager.js
+++ src/modules/PaymentTypeManager.js
@@ -1,4 +1,5 @@
 const baseUrl = "http://localhost:8000"
+const token = sessionStorage.getItem("ecommerce_token")
 
 export default{
     getPaymentTypes() {
@@ -6,9 +7,20 @@ export default{
             "method": "GET",
             "headers": {
                 "Accept": "application/json",
-                "Authorization": `Token ${sessionStorage.getItem("ecommerce_token")}`
+                "Authorization": `Token ${token}`
             }
         })
         .then(response => response.json())
+    },
+    addPayment(payment) {
+        return fetch(`${baseUrl}/payment_types`, {
+            "method": "POST",
+            "headers": {
+                "Content-Type": 'application/json',
+                "Accept": "application/json",
+                "Authorization": `Token ${token}`
+            },
+            body: JSON.stringify(payment)
+        }).then(resp => resp.json())
     }
 }
