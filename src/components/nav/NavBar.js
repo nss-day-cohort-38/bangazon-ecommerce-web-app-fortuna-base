5a6d1e6edf658a917126411560c61c5e6eca3baf
Matthew Baker <mattbaker2281@gmail.com>
2020-05-28 13:11:29 -0500

    committing for git

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -8,41 +8,47 @@ const NavBar = props => {
     const { isAuthenticated, logout } = useSimpleAuth()
 
     return (
-        <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
-            <ul className="nav nav-pills nav-fill">
-                <li className="nav-item">
-                    <Link className="nav-link" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/payment_types">My Account</Link>
-                </li>
-                {/* <li className="nav-item">
-                    <Link className="nav-link" to="/myitinerary">My Itinerary</Link>
-                </li> */}
-                {
-                    isAuthenticated() ?
-                        <li className="nav-item">
-                            <button className="nav-link fakeLink"
-                                onClick={() => {
-                                    logout()
-                                    props.history.push({
-                                        pathname: "/"
-                                    })
-                                }
-                                }
-                            >Logout</button>
-                        </li> :
-                        <>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
-                        </li>
-                        </>
-                }
-            </ul>
-        </nav>
+        <>
+            <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
+                <ul className="nav nav-pills nav-fill">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/payment_types">My Account</Link>
+                    </li>
+                    {/* <li className="nav-item">
+                        <Link className="nav-link" to="/myitinerary">My Itinerary</Link>
+                    </li> */}
+                    {
+                        isAuthenticated() ?
+                            <li className="nav-item">
+                                <button className="nav-link fakeLink"
+                                    onClick={() => {
+                                        logout()
+                                        props.history.push({
+                                            pathname: "/"
+                                        })
+                                    }
+                                    }
+                                >Logout</button>
+                            </li> :
+                            <>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login">Login</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/register">Register</Link>
+                            </li>
+                            </>
+                    }
+                </ul>
+                <form style={{marginRight: "20px"}}>
+                    <input type="text" placeholder="Search Products" style={{marginRight: "7px"}}></input>
+                    <button type="submit">Search</button>
+                </form>
+            </nav>
+        </>
     )
 }
 
