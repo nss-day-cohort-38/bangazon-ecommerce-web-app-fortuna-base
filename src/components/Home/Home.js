e5e7c4c3e11f77886adf14903a447d254c295195
ChaseSully222 <Jcsullins222@yahoo.com>
2020-05-28 13:07:24 -0500

    search bar will maintain state, working on filter functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,40 +1,49 @@
-import React, { useEffect, useState, useRef } from "react"
-import { Link }from "react-router-dom"
-import ProductManager from '../../modules/ProductManager'
-import ProductCard from './HomeProductCard'
+import React, { useEffect, useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import ProductManager from "../../modules/ProductManager";
+import ProductCard from "./HomeProductCard";
 
 const Home = (props) => {
-    const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [locationFilter, setLocationFilter] = useState([]);
 
-    const getProducts = () => {
-        return ProductManager.getAllProducts().then(ProductsFromAPI => {
-            setProducts(ProductsFromAPI)
-        })
-    }
+  const handleFieldChange = (evt) => {
+    const stateToChange = { ...locationFilter };
+    stateToChange[evt.target.id] = evt.target.value;
+    setLocationFilter(stateToChange);
+  };
 
-    useEffect(() => {
-        getProducts()
-    }, [])
+  const getProducts = () => {
+    return ProductManager.getAllProducts().then((ProductsFromAPI) => {
+      setProducts(ProductsFromAPI);
+    });
+  };
 
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-    return (
-        <>
-            <div>
-                <Link to="/sellproductform">Sell a product</Link>
-                <input class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
-            </div>
-            <div>
-                {products.map(product => (
-                    <ProductCard
-                        key={product.id}
-                        product={product}
-                        {...props}
-                    />
-                ))}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <Link to="/sellproductform">Sell a product</Link>
+        <form>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Filter by City"
+            aria-label="Filter by City"
+            onChange={handleFieldChange}
+          ></input>
+        </form>
+      </div>
+      <div>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} {...props} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-}
-
-export default Home
+export default Home;
